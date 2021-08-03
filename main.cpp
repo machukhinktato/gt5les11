50a3d958533a4e25a17858569216a06b691f1160
machukhinktato <tarabrinmv@gmail.com>
2021-08-03 10:23:49 +0300

    progressbar is done

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include <QPushButton>
 #include "checkbox.h"
 #include "listwidget.h"
+#include "progressbar.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 //    Checkbox window;
-    ListWidget window;
+//    ListWidget window;
+    ProgressBarEx window;
 
 
-//    window.resize(200, 100);
+    window.resize(500, 300);
     window.setWindowTitle("Machukhinktato");
     window.show();
     return QApplication::exec();
